0976d3c41c56d4287fb141c9caa8c338fd43501f
jonasbergstrom <jontrian97@gmail.com>
2022-09-26 20:41:57 +0200

    Initial Commit

---
+++ Important-Photos-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Important-Photos-SwiftUI
+//
+//  Created by Jonas Bergström on 2022-09-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Important-Photos-SwiftUI/Important_Photos_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Important_Photos_SwiftUIApp.swift
+//  Important-Photos-SwiftUI
+//
+//  Created by Jonas Bergström on 2022-09-26.
+//
+
+import SwiftUI
+
+@main
+struct Important_Photos_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
