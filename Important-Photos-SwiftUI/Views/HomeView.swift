ddcde2329e5fbdb613707f20c1b13cb189eac5e1
jonasbergstrom <jontrian97@gmail.com>
2022-09-27 20:01:43 +0200

    Created a List view for the "HomeView" that present the chosen image from the library

--- Important-Photos-SwiftUI/Views/HomeView.swift
+++ Important-Photos-SwiftUI/Views/HomeView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct HomeView: View {
+    @EnvironmentObject var imageData: ImageData
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List {
+            ForEach(imageData.imageNote) { note in
+                NavigationLink(destination: Text("\(note.title)")) {
+                    HStack {
+                        Image(uiImage: UIImage(data: note.image)!)
+                            .resizable()
+                            .frame(width: 50, height: 50, alignment: .center)
+                        
+                        VStack(alignment: .leading) {
+                            Text(note.title)
+                                .lineLimit(2)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .environmentObject(ImageData())
     }
 }
